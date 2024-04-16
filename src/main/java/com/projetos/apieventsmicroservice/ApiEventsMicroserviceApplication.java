1b7c95cd46254656538fe813e4f270068d60e078
Líndson Souza <146979305+lindson25@users.noreply.github.com>
2024-04-16 10:44:11 -0300

    Projeto

---
+++ src/main/java/com/projetos/apieventsmicroservice/ApiEventsMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package com.projetos.apieventsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEventsMicroserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEventsMicroserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projetos/apieventsmicroservice/ApiEventsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.apieventsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
